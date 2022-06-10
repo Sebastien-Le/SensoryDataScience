b74af6f30c5a0a3bfd7ee9e18e139b43ffc181e6
UP MATH <sebastien.le@agrocampus-ouest.fr>
2022-06-10 15:22:31 +0200

    fix

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,17 @@
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("gridExtra")
+install.packages("FactoMineR")
+install.packages("SensoMineR")
+install.packages("tidyR")
+install.packages("tibble")
+install.packages("questionr")
+install.packages("readxl")
+install.packages("vietnameseConverter")
+install.packages("stringi")
+install.packages("factoextra")
+
+
+
+
+
